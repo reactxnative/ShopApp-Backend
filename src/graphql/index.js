ada1ec91ef17f032a4a559015e0e7cc2b238a8e8
Rahul Singh <reactxnative@gmail.com>
2026-07-10 13:42:14 +0530

    Add Apollo Server plugins for local and production landing pages

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,10 +1,25 @@
 const { ApolloServer } = require("@apollo/server");
+const {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} = require("@apollo/server/plugin/landingPage/default");
+
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
+  plugins: [
+    // process.env.NODE_ENV === "production"
+    //   ? ApolloServerPluginLandingPageProductionDefault({
+    //       embed: true,
+    //     })
+      ApolloServerPluginLandingPageLocalDefault({
+          embed: true,
+        }),
+  ],
 });
 
 module.exports = server;
